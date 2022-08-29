a5f52ebe8773d5e8857c75b862cf0f3be91ae06e
enemysfriend22 <55262034+enemysfriend22@users.noreply.github.com>
2022-08-29 17:43:07 +0200

    Adds Vial for the Rama Kara
    Adds basic Vial support. RGB works but is not configurable within Vial

---
+++ keyboards/wilba_tech/rama_works_kara/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xF8, 0x18, 0xD0, 0x67, 0x6D, 0x55, 0xBA, 0xF0}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 12 }
